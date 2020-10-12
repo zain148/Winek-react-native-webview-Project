6f5b1defa6b4a49bca882a9b74a766c725448178
zain148 <zainarshad028@gmail.com>
2020-10-13 00:41:50 +0500

    Done

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { View, ActivityIndicator, SafeAreaView, StyleSheet, Platform } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, 2500);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <>
+        {loading === true ? (
+          <View style={{ justifyContent: "center", alignItems: "center" }}>
+            <ActivityIndicator size="large" color="black" />
+          </View>
+        ) : null}
+
+        <WebView source={{ uri: "https://gsh4.net/id10/mobile/" }} />
+      </>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === "android" ? 30 : 0,
   },
 });
